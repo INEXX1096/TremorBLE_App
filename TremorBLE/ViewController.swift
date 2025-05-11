982a8f789ab9593e02e96388e78db3654cf86d58
Zihao Huang <zihaoeric@icloud.com>
2025-05-10 22:09:54 -0400

    Initial Commit

---
+++ TremorBLE/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TremorBLE
+//
+//  Created by 黄子豪 on 2025/5/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
